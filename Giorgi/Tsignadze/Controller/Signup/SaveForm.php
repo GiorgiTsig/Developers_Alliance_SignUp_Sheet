4eeba4574f68ce3d1cb28a8a2d6e916e81eafe07
GiorgiTsig <giorgi.tsignadze.1@btu.edu.ge>
2022-09-06 20:40:19 +0400

    Add existing project files to Git

---
+++ Giorgi/Tsignadze/Controller/Signup/SaveForm.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Giorgi\Tsignadze\Controller\Signup;
+
+use Magento\Framework\App\Action\HttpPostActionInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\Exception\NotFoundException;
+
+class SaveForm implements HttpPostActionInterface
+{
+    /**
+     * Execute action based on request and return result
+     *
+     * @return ResultInterface|ResponseInterface
+     * @throws NotFoundException
+     */
+    public function execute()
+    {
+        // TODO: Implement execute method.
+    }
+}
